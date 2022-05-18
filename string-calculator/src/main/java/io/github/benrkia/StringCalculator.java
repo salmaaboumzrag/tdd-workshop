1641ce6b3821e43ecfbb321d5b8cef8de3558ba7
ilyasse benrkia <ilyasse.benrkia@octo.ma>
2022-05-18 14:23:53 +0100

    init

---
+++ string-calculator/src/main/java/io/github/benrkia/StringCalculator.java
@@ -0,0 +1,4 @@
+package io.github.benrkia;
+
+public class StringCalculator {
+}
---
+++ string-calculator/src/test/java/io/github/benrkia/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+package io.github.benrkia;
+
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+  @Test
+  void test() {
+
+  }
+}
